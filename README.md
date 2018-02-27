919ecf79ab1aa60b6f525177e6e2a5b614652e77
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-02-27 07:34:00 -0800

    React Scaffold.

---
+++ README.md
@@ -0,0 +1,24 @@
+## BITBOX Scaffold React
+
+### Intro
+
+BITBOX scaffolds are basic apps w/ bindings to your local running BITBOX. They allow a developer to hit the ground running and waste no time w/ app set up and configuration.
+
+This scaffold is a React app build w/ [Facebook's `create-react-app`](https://github.com/facebook/create-react-app). It already has `BITBOX` instantiated and is ready to roll.
+
+## Setup
+
+1. Download the latest build from [bitbox.earth](https://www.bitbox.earth/) and compare [the checksums](https://github.com/bigearth/keys-n-hashes)
+2. Start your local BITBOX
+3. [Install `bitbox-cli`](https://www.npmjs.com/package/bitbox-cli) globally
+  * `npm install bitbox-cli --global`
+4. Create empty directory for your new app
+  * `mkdir BCH4all && cd BCH4all`
+5. Scaffold a React app w/ BITBOX web bindings
+  * `bitbox scaffold react`
+6. Install dependencies
+  * `npm install`
+7. Start the app
+  * `npm start`
+8. Open a browser to `http://localhost:3000/` and confirm you are seeing the `getinfo` method returning data from your local BITBOX
+9. Win
---
+++ src/App.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+
+import './App.css';
+let BITBOXCli = require('bitbox-cli/lib/bitboxcli').default;
+let BITBOX = new BITBOXCli({
+  protocol: 'http',
+  host: '127.0.0.1',
+  port: 8332,
+  username: '',
+  password: ''
+});
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      version: '',
+      protocolversion: '',
+      walletversion: '',
+      balance: '',
+      blocks: '',
+      timeoffset: '',
+      connections: '',
+      proxy: '',
+      difficulty: '',
+      testnet: '',
+      keypoololdest: '',
+      keypoolsize: '',
+      paytxfee: '',
+      relayfee: '',
+      errors: ''
+    }
+  }
+
+  componentDidMount() {
+    BITBOX.getinfo()
+    .then((result) => {
+      this.setState({
+        version: result.version,
+        protocolversion: result.protocolversion,
+        walletversion: result.walletversion,
+        balance: result.balance,
+        blocks: result.blocks,
+        timeoffset: result.timeoffset,
+        connections: result.connections,
+        proxy: result.proxy,
+        difficulty: result.difficulty,
+        testnet: result.testnet,
+        keypoololdest: result.keypoololdest,
+        keypoolsize: result.keypoolsize,
+        paytxfee: result.paytxfee,
+        relayfee:result.relayfee,
+        errors: result.errors
+      });
+    }, (err) => { console.log(err);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Hello BITBOX</h1>
+        </header>
+        <div className='App-content'>
+          <h2><code>getinfo</code></h2>
+          <ul>
+            <li>
+                version: {this.state.version},
+            </li>
+            <li>
+                protocolversion: {this.state.protocolversion},
+            </li>
+            <li>
+                walletversion: {this.state.walletversion},
+            </li>
+            <li>
+                balance: {this.state.balance},
+            </li>
+            <li>
+                blocks: {this.state.blocks},
+            </li>
+            <li>
+                timeoffset: {this.state.timeoffset},
+            </li>
+            <li>
+                connections: {this.state.connections},
+            </li>
+            <li>
+                proxy: {this.state.proxy},
+            </li>
+            <li>
+                difficulty: {this.state.difficulty},
+            </li>
+            <li>
+                testnet: {this.state.testnet},
+            </li>
+            <li>
+                keypoololdest: {this.state.keypoololdest},
+            </li>
+            <li>
+                keypoolsize: {this.state.keypoolsize},
+            </li>
+            <li>
+                paytxfee: {this.state.paytxfee},
+            </li>
+            <li>
+                relayfee:{this.state.relayfee},
+            </li>
+            <li>
+                errors: {this.state.errors}
+            </li>
+            </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
