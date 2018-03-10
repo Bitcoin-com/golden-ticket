a1ebaba5f66941565fe63ea8e65b338bbe284ffa
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-03-09 19:20:44 -0800

    bitbox.js

---
+++ bitbox.js
@@ -0,0 +1,11 @@
+exports.config = {
+  networks: {
+    development: {
+      protocol: "http",
+      host: "localhost",
+      port: "8332",
+      username: "",
+      password: ""
+    }
+  }
+};
