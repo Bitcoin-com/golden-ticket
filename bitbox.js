c2d7d7435aa96110f944751a5087225a8c4269ff
Gabriel Cardona <gabriel@bitcoin.com>
2018-07-05 10:06:29 +0900

    v and bitbox.js

--- bitbox.js
+++ bitbox.js
@@ -1,6 +1,9 @@
 exports.config = {
   networks: {
     development: {
+      restURL: "https://trest.bitbox.earth/v1/"
+    },
+    production: {
       restURL: "https://rest.bitbox.earth/v1/"
     }
   }
