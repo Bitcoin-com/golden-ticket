35cdafe62cd67d679f0bc67608344e10e0a9662c
Gabriel Cardona <gabriel@bitcoin.com>
2019-01-03 14:14:45 -0800

    Update code and readme for generate-wallet.

--- README.md
+++ README.md
@@ -37,17 +37,13 @@ Create a 256 bit mnemonic in any of the following languages
 
 `npm run generate-wallet`
 
-You'll be prompted for a language. You can enter any of the 8 listed above. If you don't enter a language Golden Ticket will default to English.
+You'll be prompted for a language. You can enter any of the 8 listed above. If you don't enter a language Golden Ticket will default to english.
 
 Next you'll be promted for an HDpath. If you enter nothing Golden Ticket will default to the first BIP44 account of your mnemonic. ie: `m/44'/145'/0'`
 
-Then you'll be prompted for the number of tickets that you intend to fund. It will default to 100.
+Finally you'll be prompted for a wallet name. Don't include the file extension. If you enter nothing it will default to a `wallet.json` file and write to it your mnemonic, hdPath and mothership address.
 
-You'll also be prompted for your tiers. This is how much $ you want to give to `n` number of addresses. For example if you want to give $1 to 99 tickets and $10 to 1 goldend ticket your tiers would be `[[1, 99], [10, 1]]`
-
-Golden Ticket will create a `wallet.json` file and write to it your mnemonic, mothership address, number of tickets and HD path.
-
-Lastly Golden Ticket will show you a QR code of your mothership address. The HD path for the mother ship address is `hdpath/1/0`.
+Lastly Golden Ticket will show you a QR code of your mothership address. The HD path for the mother ship address is `hdpath/1/0`. The mothership address is the address which you intially fund which then funds all the generated addresses.
 
 ![generate-wallet](images/generate-wallet.png)
 
@@ -64,10 +60,3 @@ Run `npm run redeem-unclaimed-funds` to have funds sent back to the funders cash
 ## Generate Stats
 
 Run `npm run generate-stats` to generate basic stats about your event.
-
-```
-Ticket count: Total 100, claimed 75, unclaimed 25
-$1 breakdown: claimed 80, unclaimed 19
-$10 breakdown: claimed 1, unclaimed 0
-Total funds claimed: claimed 81, unclaimed 19
-```
--- generate-wallet.js
+++ generate-wallet.js
@@ -1,94 +1,73 @@
-const BITBOXCli = require("bitbox-cli/lib/bitbox-cli").default;
-const BITBOX = new BITBOXCli();
-const fs = require("fs");
-const qrcode = require("qrcode-terminal");
-const emoji = require("node-emoji");
-const chalk = require("chalk");
-const prompt = require("prompt");
+"use strict"
+const BITBOXCli = require("bitbox-cli/lib/bitbox-cli").default
+const BITBOX = new BITBOXCli()
+const fs = require("fs")
+const qrcode = require("qrcode-terminal")
+const emoji = require("node-emoji")
+const chalk = require("chalk")
+const prompt = require("prompt")
 
-console.log(chalk.green("All done."), emoji.get(":white_check_mark:"));
+const main = async () => {
+  // start the prompt to get user input
+  prompt.start()
 
-let main = async () => {
-  prompt.start();
+  // ask for language, hdpath and walletFileName
+  prompt.get(["language", "hdpath", "walletFileName"], (err, result) => {
+    // generate mnemonic based on language input. Default to english
+    const mnemonic = BITBOX.Mnemonic.generate(
+      256,
+      BITBOX.Mnemonic.wordLists()[
+        result.language ? result.language.toLowerCase() : "english"
+      ]
+    )
+    // show the user their mnemoninc
+    console.log(`Your mnemonic is: ${chalk.red(mnemonic)}`)
 
-  prompt.get(
-    ["language", "hdpath", "numberOfTickets", "tiers"],
-    (err, result) => {
-      let mnemonic = BITBOX.Mnemonic.generate(
-        256,
-        BITBOX.Mnemonic.wordLists()[
-          result.language ? result.language.toLowerCase() : "english"
-        ]
-      );
-      console.log(`Your mnemonic is: ${chalk.red(mnemonic)}`);
+    // root seed buffer
+    const rootSeed = BITBOX.Mnemonic.toSeed(mnemonic)
 
-      // root seed buffer
-      const rootSeed = BITBOX.Mnemonic.toSeed(mnemonic);
+    // master HDNode
+    const masterHDNode = BITBOX.HDNode.fromSeed(rootSeed)
 
-      // master HDNode
-      const masterHDNode = BITBOX.HDNode.fromSeed(rootSeed);
+    // set the hdpath based on input. Default to BCH BIP44
+    let hdpath
+    if (result.hdpath) hdpath = result.hdpath
+    else hdpath = "m/44'/145'/0'"
 
-      let hdpath;
-      if (result.hdpath) {
-        hdpath = result.hdpath;
-      } else {
-        hdpath = "m/44'/145'/0'";
-      }
+    console.log(`Your account's HDPath is ${hdpath}`)
+    // HDNode of BIP44 account
+    const account = BITBOX.HDNode.derivePath(masterHDNode, hdpath)
 
-      console.log(`Your account's HDPath is ${hdpath}`);
-      // HDNode of BIP44 account
-      const account = BITBOX.HDNode.derivePath(masterHDNode, hdpath);
+    // HDNode of first internal change address
+    const mothership = BITBOX.HDNode.derivePath(account, `1/0`)
+    console.log(`Your mothership's HDPath is ${hdpath}/1/0`)
 
-      // HDNode of first internal change address
-      const mothership = BITBOX.HDNode.derivePath(account, `1/0`);
-      console.log(`Your mothership's HDPath is ${hdpath}/1/0`);
+    // mothership HDNode to cashAddr
+    const mothershipAddress = BITBOX.HDNode.toCashAddress(mothership)
 
-      // mothership HDNode to cashAddr
-      const mothershipAddress = BITBOX.HDNode.toCashAddress(mothership);
+    // show mothership address qr code
+    console.log(`Fund the mothership at: ${mothershipAddress}\n`)
+    qrcode.generate(mothershipAddress)
 
-      let numberOfTickets;
-      if (result.numberOfTickets) {
-        numberOfTickets = result.numberOfTickets;
-      } else {
-        numberOfTickets = 100;
-      }
-
-      let tiers;
-      if (result.tiers) {
-        tiers = result.tiers;
-      } else {
-        tiers = [[1, numberOfTickets]];
-      }
-
-      console.log(`Your tiers are: ${tiers}`);
-
-      // show mothership address qr code
-      console.log(`Fund the mothership at: ${mothershipAddress}\n`);
-      qrcode.generate(mothershipAddress);
-
-      // mnemonic and mothership address to save in basic wallet
-      let mnemonicObj = {
-        mnemonic: mnemonic,
-        hdpath: hdpath,
-        tiers: tiers,
-        mothershipAddress: mothershipAddress,
-        numberOfTickets: numberOfTickets
-      };
-
-      // Write out the basic wallet into a json file for other scripts  to use.
-      fs.writeFile(
-        "wallet1.json",
-        JSON.stringify(mnemonicObj, null, 2),
-        err => {
-          if (err) return console.error(err);
-          console.log(
-            emoji.get(":rocket:"),
-            `wallet.json written successfully.`
-          );
-        }
-      );
+    // mnemonic, hdpath and mothership address to save in basic wallet
+    const mnemonicObj = {
+      mnemonic: mnemonic,
+      hdpath: hdpath,
+      mothershipAddress: mothershipAddress
     }
-  );
-};
 
-main();
+    // get walletFileName from user. Default to wallet.json
+    let wfn = "goldenTicketWallet.json"
+    if (result.walletFileName) wfn = `${result.walletFileName}.json`
+
+    // Write out the basic wallet into a json file for other scripts  to use.
+    fs.writeFile(`${wfn}`, JSON.stringify(mnemonicObj, null, 2), err => {
+      if (err) return console.error(err)
+
+      console.log(chalk.green("All done."), emoji.get(":white_check_mark:"))
+      console.log(emoji.get(":rocket:"), `${wfn} written successfully.`)
+    })
+  })
+}
+
+main()
