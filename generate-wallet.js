839aba830c16255cafbf69ef8edbfc5ebdd29c61
Gabriel Cardona <gabriel@bitcoin.com>
2019-03-20 06:34:04 -0700

    Change wallet format.

--- generate-wallet.js
+++ generate-wallet.js
@@ -34,13 +34,11 @@ const main = async () => {
     if (result.hdpath) hdpath = result.hdpath
     else hdpath = "m/44'/145'/0'"
 
-    console.log(`Your account's HDPath is ${hdpath}`)
-    // HDNode of BIP44 account
-    const account = BITBOX.HDNode.derivePath(masterHDNode, hdpath)
+    const mothershipHDPath = "m/44'/145'/1'/0/0"
 
     // HDNode of first internal change address
-    const mothership = BITBOX.HDNode.derivePath(account, `1/0`)
-    console.log(`Your mothership's HDPath is ${hdpath}/1/0`)
+    const mothership = BITBOX.HDNode.derivePath(masterHDNode, mothershipHDPath)
+    console.log(`Your mothership's HDPath is ${mothershipHDPath}`)
 
     // mothership HDNode to cashAddr
     const mothershipAddress = BITBOX.HDNode.toCashAddress(mothership)
@@ -52,8 +50,10 @@ const main = async () => {
     // mnemonic, hdpath and mothership address to save in basic wallet
     const mnemonicObj = {
       mnemonic: mnemonic,
-      hdpath: hdpath,
-      mothershipAddress: mothershipAddress
+      mothership: {
+        hdPath: mothershipHDPath,
+        address: mothershipAddress
+      }
     }
 
     // get walletFileName from user. Default to wallet.json
