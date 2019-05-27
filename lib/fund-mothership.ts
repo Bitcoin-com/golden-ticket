bb50a5a3de1b45eba7d1a6585f2ff1b53ad7f9f9
Gabriel Cardona <gabriel@bitcoin.com>
2019-05-27 11:33:28 +0900

    Clean up deps. Port fund-mothership to .ts.

---
+++ lib/fund-mothership.ts
@@ -0,0 +1,22 @@
+// imports
+import { Wallet } from "./interfaces/GoldenTicketInterfaces"
+
+// consts
+const qrcode = require("qrcode-terminal")
+const chalk = require("chalk")
+const emoji = require("node-emoji")
+
+try {
+  const wallet: Wallet = require(`./goldenTicketWallet.json`)
+
+  // show funder address qr code
+  console.log(`Send funds to: ${wallet.mothership.address}`)
+  qrcode.generate(wallet.mothership.address)
+} catch (err) {
+  console.log(
+    `Could not open goldenTicketWallet.json. Generate a mnemonic with generate-wallet first.
+      Exiting.`
+  )
+  process.exit(0)
+}
+console.log(chalk.green("All done."), emoji.get(":white_check_mark:"))
--- lib/interfaces/GoldenTicketInterfaces.ts
+++ lib/interfaces/GoldenTicketInterfaces.ts
@@ -2,3 +2,12 @@ export interface GenerateWalletResult {
   language: string
   walletFileName: string
 }
+
+export interface Wallet {
+  mnemonic: string
+  hdpath: string
+  mothership: {
+    hdPath: string
+    address: string
+  }
+}
